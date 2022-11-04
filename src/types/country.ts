5a25050ac7f65290b00be0aa7c828553913ac0df
Danyil Kokhnenko <kokhnenko8@gmail.com>
2022-11-04 02:43:37 +0200

    Update country.ts

---
+++ src/types/country.ts
@@ -0,0 +1,21 @@
+export interface country {
+    name: {
+        official: string
+    },
+    currencies: {
+        [key:string]:{
+            name: string,
+            symbol: string
+        }
+    },
+    capital: string[],
+    region: string,
+    languages: {
+        [key:string]: string
+    },
+    flags: {
+        png: string,
+        svg: string
+    }
+
+}
